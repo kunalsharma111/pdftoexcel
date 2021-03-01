e6db38a45cc633c3e255478ee62b57faea1e26fe
kunalsharma111 <sharmakunal315@gmail.com>
2021-03-01 18:24:24 +0530

    pdf to excel

---
+++ index.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const pdfparse = require('pdf-parse');
+
+const xlsx = require('xlsx');
+const path = require('path');
+
+const pdffile = fs.readFileSync('test.pdf');
+let arr=[];
+pdfparse(pdffile).then(function (data){
+    let newData = JSON.stringify(data).replace(/\\n/g, '');
+    temp = "";
+    flag = 0;
+    for(let i=0;i<newData.length;i++){
+        if(newData[i] == "(" && newData[i+1] == "C" && newData[i+2] == "L"){
+            if(flag == 0){ flag = 1;}
+            else if(flag == 1){
+                arr.push(temp);
+                temp = "";
+                flag = 0; 
+            }
+        }
+        if(flag == 1){
+            temp = temp + newData[i];
+        }
+    }
+
+
+
+    hindu = [];
+    jatsikh = [];
+    divorcee = [];
+    doctorEngineer = [];
+    nri = [];
+    allLeft = [];
+    for(let i=0;i<arr.length;i++){
+        if(arr[i].includes("hindu") || arr[i].includes("Hindu")){
+            hindu.push(arr[i]);
+        }else if(arr[i].includes("divorcee") || arr[i].includes("Divorcee")){
+            divorcee.push(arr[i]);
+        }else if(arr[i].includes("jat") || arr[i].includes("sikh") || arr[i].includes("Jat")|arr[i].includes("Sikh")){
+            jatsikh.push(arr[i]);
+        }else if(arr[i].includes("doctor") || arr[i].includes("engineer")){
+            doctorEngineer.push(arr[i]);
+        }else if(arr[i].includes("nri") || arr[i].includes("NRI")){
+            nri.push(arr[i]);
+        }else{
+            allLeft.push(arr[i]);
+        }
+    }
+    console.log(hindu.length);
+    console.log(divorcee.length);
+    console.log(jatsikh.length);
+    console.log(doctorEngineer.length);
+    console.log(allLeft.length);
+    const hdata = hindu.map(h => {
+        return [h];
+    })
+    console.log(hdata);
+    const workBook = xlsx.utils.book_new();
+    const workSheetData = [
+        ['list'],
+        hdata
+    ];
+    filePath = './outputFiles/list.xlsx';
+    const workSheet = xlsx.utils.aoa_to_sheet(workSheetData);
+    xlsx.utils.book_append_sheet(workBook,workSheet,"list");
+    xlsx.writeFile(workBook,path.resolve(filePath));
+})
